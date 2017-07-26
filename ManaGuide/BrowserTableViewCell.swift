43e6cf5123faaf52127633d6b584c8d49edbd1a9
Jovito Royeca <jovit.royeca@gmail.com>
2017-07-27 01:50:38 +0800

    Pod updates.

---
+++ ManaGuide/BrowserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrowserTableViewCell.swift
+//  ManaGuide
+//
+//  Created by Jovito Royeca on 26/07/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class BrowserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
