be5aab4cfab1cb67cc511a089283ab9134e0bc24
Jovito Royeca <jovit.royeca@gmail.com>
2017-08-08 01:04:57 +0800

    Pod updates.

---
+++ ManaGuide/DynamicHeightTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DynamicHeightTableViewCell.swift
+//  ManaGuide
+//
+//  Created by Jovito Royeca on 07/08/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class DynamicHeightTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
