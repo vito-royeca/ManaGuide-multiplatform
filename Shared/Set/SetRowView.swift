6325ddc04c5a8c22b29f57e098b75c5afec193ee
Vito Royeca <9709494+vito-royeca@users.noreply.github.com>
2023-11-19 15:56:44 -0500

    Redesigned the SetRowView

--- Shared/Set/SetRowView.swift
+++ Shared/Set/SetRowView.swift
@@ -17,11 +17,101 @@ struct SetRowView: View {
     let style: SetRowViewStyle
 
     var body: some View {
+        if style == .listRow {
+            listRowView
+        } else if style == .header {
+            headerView
+        }
+    }
+    
+    private var listRowView: some View {
+        HStack(alignment: .center, spacing: 10) {
+            if let url = set.smallLogoURL {
+                CacheAsyncImage(url: url) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipped()
+                    } else {
+                        EmptyView()
+                    }
+                }
+                .frame(width: 100, height: 50)
+            } else {
+                Image(uiImage: ManaKit.shared.image(name: .mtgLogo)!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 50)
+            }
+
+            VStack(alignment: .leading) {
+                Text(set.name ?? "")
+                    .font(.subheadline)
+                infoView
+            }
+        }
+    }
+    
+    private var headerView: some View {
+        VStack(alignment: .center) {
+            if let url = set.bigLogoURL {
+                CacheAsyncImage(url: url) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipped()
+                    } else {
+                        EmptyView()
+                    }
+                }
+                .frame(maxHeight: 100)
+            } else {
+                Image(uiImage: ManaKit.shared.image(name: .mtgLogo)!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: 100)
+            }
+
+            HStack {
+                Text(set.name ?? "")
+                    .font(.title3)
+                Spacer()
+            }
+
+            detailedInfoView
+        }
+    }
+
+    private var infoView: some View {
+        HStack {
+            Text(set.keyrune2Unicode)
+                .font(Font.custom("Keyrune", size: 20))
+            Text((set.code).uppercased())
+                .font(.subheadline)
+                .foregroundColor(Color.gray)
+            Spacer()
+            Text("\(set.cardCount) cards")
+                .font(.subheadline)
+                .foregroundColor(Color.gray)
+        }
+    }
+
+    private var detailedInfoView: some View {
         VStack(spacing: 10) {
-            if style == .listRow {
-                listRowView
-            } else if style == .header {
-                headerView
+            HStack {
+                Text("Type")
+                    .font(.footnote)
+                    .foregroundColor(Color.gray)
+                Text(set.setType?.name ?? "")
+                    .font(.subheadline)
+                Spacer()
+                Text("Block")
+                    .font(.footnote)
+                    .foregroundColor(Color.gray)
+                Text(set.setBlock?.name ?? String.emdash)
+                    .font(.subheadline)
             }
 
             HStack {
@@ -56,73 +146,6 @@ struct SetRowView: View {
                     Text("\(set.cardCount)")
                         .font(.subheadline)
                 }
-            }
-        }
-    }
-    
-    var listRowView: some View {
-        HStack {
-            if let url = set.smallLogoURL {
-                CacheAsyncImage(url: url) { phase in
-                    if let image = phase.image {
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .clipped()
-                    } else {
-                        EmptyView()
-                    }
-                }
-                .frame(width: 100, height: 50)
-            } else {
-                Image(uiImage: ManaKit.shared.image(name: .mtgLogo)!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 50)
-            }
-            Text(set.name ?? "")
-                .font(.title3)
-            Spacer()
-        }
-    }
-    
-    var headerView: some View {
-        VStack(alignment: .center) {
-            if let url = set.bigLogoURL {
-                CacheAsyncImage(url: url) { phase in
-                    if let image = phase.image {
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .clipped()
-                    } else {
-                        EmptyView()
-                    }
-                }
-                .frame(maxHeight: 100)
-            } else {
-                Image(uiImage: ManaKit.shared.image(name: .mtgLogo)!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxHeight: 100)
-            }
-            HStack {
-                Text(set.name ?? "")
-                    .font(.title3)
-                Spacer()
-            }
-            HStack {
-                Text("Type")
-                    .font(.footnote)
-                    .foregroundColor(Color.gray)
-                Text(set.setType?.name ?? "")
-                    .font(.subheadline)
-                Spacer()
-                Text("Block")
-                    .font(.footnote)
-                    .foregroundColor(Color.gray)
-                Text(set.setBlock?.name ?? String.emdash)
-                    .font(.subheadline)
             }
         }
     }
